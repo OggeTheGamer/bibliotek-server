fee02ba442a389450dfd8d8d9d733536045f4298
Movitz Sunar <movitz.sunar@gmail.com>
2018-10-21 15:02:32 +0200

    Enabled logout, fixed head, added js cookie

--- Biblotek/src/main/java/nu/ssis/a18mosu/security/SecurityConfiguration.java
+++ Biblotek/src/main/java/nu/ssis/a18mosu/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.context.request.RequestContextListener;
 
 @Configuration
@@ -17,8 +18,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	public void configure(HttpSecurity http) throws Exception {
 		// @formatter:off
 		http
+			.logout()
+				.logoutUrl("/logout")
+				.logoutSuccessUrl("/")
+				.permitAll()
+		.and()
+			.csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+		.and()
 			.authorizeRequests()
-				.antMatchers("/admin/**").hasRole("ADMIN")
+				.antMatchers("/admin", "/admin/**").hasRole("ADMIN")
+				.antMatchers("/user", "/user/**").hasRole("USER")
 		.and()
 			.oauth2Login()
 				.userInfoEndpoint()
